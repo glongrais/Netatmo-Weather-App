795a091be3544cccaf05f79b7772afe0b961edec
Guillaume Longrais <guillaume.longrais@orange.fr>
2023-04-06 14:59:52 +0200

    Initial Commit

---
+++ WeatherAppMac/WeatherAppMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppMacApp.swift
+//  WeatherAppMac
+//
+//  Created by Guillaume Longrais on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
