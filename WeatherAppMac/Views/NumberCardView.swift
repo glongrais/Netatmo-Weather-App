a45ca24c30f1f3d8042196e16966343611133daf
Guillaume Longrais <guillaume.longrais@orange.fr>
2023-05-05 17:47:34 +0200

    Updated number view

--- WeatherAppMac/Views/NumberCardView.swift
+++ WeatherAppMac/Views/NumberCardView.swift
@@ -9,27 +9,29 @@ import SwiftUI
 
 struct NumberCardView<T: LosslessStringConvertible>: View {
     var value: T
-    var label: String = ""
+    var label: String
+    var color: Color = .black
     var body: some View {
-        ZStack{
+        VStack {
+            Text(label)
+                .foregroundColor(.black)
+            Text(String(value))
+                .foregroundColor(color)
+                .bold()
+        }
+        .padding(8)
+        .background(
             RoundedRectangle(cornerRadius: 10)
                 .foregroundColor(Color.white)
-                .frame(width: 80, height: 60)
-            VStack{
-                Text(label)
-                    .foregroundColor(.black)
-                Text(String(value)) // Convert value to a String
-                    .foregroundColor(.black)
-            }
-        }
+        )
     }
 }
 
 struct NumberCardView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            NumberCardView(value: 10, label: "Temperature (Int)")
-            NumberCardView(value: 10.5, label: "Temperature (Double)")
+            NumberCardView(value: 10, label: "Temperature (Int)", color: .black)
+            NumberCardView(value: 10.5, label: "Temperature (Double)", color: .blue)
         }
     }
 }
