a57515d3e08a7dfc9e5c94174a22da0ea7d83e41
Guillaume Longrais <guillaume.longrais@orange.fr>
2023-05-07 20:48:40 +0200

    Skeleton credentials

---
+++ WeatherAppMac/Models/NetatmoCredentials.swift
@@ -0,0 +1,14 @@
+//
+//  NetatmoCredentials.swift
+//  WeatherAppMac
+//
+//  Created by Guillaume Longrais on 2023-04-06.
+//
+
+struct NetatmoCredentials {
+    static var accessToken = "YOUR_ACCESS_TOKEN"
+    static let refreshToken = "YOUR_REFRESH_TOKEN"
+    static let clientId = "YOUR_CLIENT_ID"
+    static let clientSecret = "YOUR_CLENT_SECRET"
+    static let deviceId = "YOUR_DEVICE_ID"
+}
